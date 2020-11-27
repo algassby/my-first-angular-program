4ab484906cb0d406d4a71e3288a32793ef70ef36
algassby <algassby@hotmail.com>
2020-11-27 22:39:30 +0100

    creation d'un observable à travers rxjs , Observable et subscribe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
 import { AppareilService } from './services/appareil.service';
 
 @Component({
@@ -6,11 +8,27 @@ import { AppareilService } from './services/appareil.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
  
+  secondes:number =0;
  constructor(){
    
  }
+  ngOnInit(): void {
+   const count = Observable.interval(1000);
+   //subscribe prend trois methode , subscribe permet de s'abonner à un observable
+   count.subscribe(
+     (value:number)=>{
+       this.secondes =value;
+     },
+     (error:any)=>{
+       console.log("Error");
+     },
+     ()=>{
+      console.log("Observable completé");
+     }
+   )
+  }
 
 
 
